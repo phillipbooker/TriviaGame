30f874a50128b667691698997acb42768d1d9576
Phillip Booker <phillippbooker@gmail.com>
2019-04-04 23:27:01 -0700

    Adds pictures for all questions. Uses addToField function.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,21 +2,21 @@
 var questionBank = [];
 var gameQuestion;
 var gameState;
-
 var rightCount;
 var wrongCount;
 
-var playerGuess;
+//Boolean - if true, player guessed correctly
+var playerCorrect;
 
+//Boolean - if true, time expired
 var timeUp;
 
+//Holds timeout ID for question display and answer display respectively
 var questionTimer;
 var answerTimer;
 
-var ANSWERS_MAX = 4;
-//var correctAnswer;
 
-//Classes
+//Answer class
 class Answer {
     constructor(id, answer, correct) {
         this.id = id;
@@ -25,13 +25,15 @@ class Answer {
     }
 }
 
-//Needs a picture
+//Question class - holds answers and a picture
 class Question {
-    constructor(question, answers) {
+    constructor(question, answers, picture) {
         this.question = question;
         this.answers = answers;
+        this.picture = picture;
     }
 
+    //Returns the correct answer to the question
     correctAnswer(){
         var right;
         $.each(this.answers, function(i, answer){
@@ -65,6 +67,7 @@ function shuffle(array) {
     return array;
 }
 
+//Creates an element using 'tag' to add to the question-field div - can give it a custom class
 function addToField(tag, divClass, text){
     var questionFieldElement = $("<" + tag + ">");
     questionFieldElement.addClass(divClass);
@@ -73,7 +76,7 @@ function addToField(tag, divClass, text){
 }
 
 //Takes an array of answers for a question (first answer is correct)
-function makeQuestion(question, answers){
+function makeQuestion(question, answers, picture){
     var answersPush = [];
     var correctAnswer = new Answer("c", answers[0], true);
 
@@ -84,10 +87,11 @@ function makeQuestion(question, answers){
         answersPush.push(wrongAnswer);
     }
     
-    var question = new Question(question, answersPush);
+    var question = new Question(question, answersPush, picture);
     return(question);
 }
 
+//When player is out of time set timeUp to true and increase wrongCount
 function outOfTime(){
     gameState = 3;
     timeUp = true;
@@ -95,64 +99,60 @@ function outOfTime(){
     resolveAnswer();
 }
 
+//Initialize game
 function resetGame(){
-    // $("#question").text("");
-    // for(var i = 0; i < ANSWERS_MAX; i++){
-    //     $("#answer-" + (i+1)).text("");
-    // }
+    //Empty the game field
     $("#question-field").empty();
 
     rightCount = 0;
     wrongCount = 0;
+    timeUp = false;
 
+    //Tell user to click in the game field to start game
     var clickHere = $("<h2>");
     clickHere.text("Click here to start!")
     $("#question-field").append(clickHere);
 
+    //Initialize the question bank
     questionBank = [];
-    questionBank.push(makeQuestion("What is the name of the monkey on King Kai's planet?", ["Bubbles", "Gregory", "Puar", "Oolong"]));
-    questionBank.push(makeQuestion("Who killed Krillin on Planet Namek?", ["Freiza", "Piccolo", "Vegeta", "Captain Ginyu"]));
-    questionBank.push(makeQuestion("Who is known as the Prince of all Sayians?", ["Vegeta", "Goku", "Raditz", "Broly"]));
+    questionBank.push(makeQuestion("What is the name of the monkey on King Kai's planet?", ["Bubbles", "Gregory", "Puar", "Oolong"], "assets/images/bubbles.png"));
+    questionBank.push(makeQuestion("Who killed Krillin on Planet Namek?", ["Freiza", "Piccolo", "Vegeta", "Captain Ginyu"], "assets/images/frieza.gif"));
+    questionBank.push(makeQuestion("Who is known as the Prince of all Sayians?", ["Vegeta", "Goku", "Raditz", "Broly"], "assets/images/vegeta.jpeg"));
 
     //Shuffle the question bank
     questionBank = shuffle(questionBank);
 
-    timeUp = false;
-
+    //Game initialized state
     gameState = 1;
     $("#game-state").text("Game state: " + gameState);
 }
 
-function displayQuestion(question){
+//Shows question in game field by adding divs to quesiton-field
+function displayQuestion(questionPass){
+    //Question display state
     gameState = 2;
     $("#game-state").text("Game state: " + gameState);
 
-    var gameAnswers = question.answers;
+    //Shuffles the order the answers appear in
+    var gameAnswers = questionPass.answers;
     gameAnswers = shuffle(gameAnswers);
 
-    //Display question
-    var questionDiv = $("<div>");
-    questionDiv.addClass("game-question");
-    questionDiv.text(question.question);
-    $("#question-field").append(questionDiv);
-
-    // $("#question").text(question.question);
+    //Display question in game field
+    $("#question-field").append(addToField("div", "game-question", questionPass.question));
     
     //Display answers
-    $.each(gameAnswers, function(i, answer){
-        // $("#answer-" + (i+1)).text(answer.answer);
-        var answerDiv = $("<div>");
-        answerDiv.addClass("game-answer");
-        answerDiv.text(answer.answer);
-        $("#question-field").append(answerDiv);
+    $.each(gameAnswers, function(i, answerLoop){
+        $("#question-field").append(addToField("div", "game-answer", answerLoop.answer));
     });
     
+    //Kick off the question timer
     questionTimer = setTimeout(outOfTime, 1000 * 10);
     console.log("question displayed: " + questionTimer);
 }
 
-//Choose a game question
+//Chooses a game question from the bank
 function setQuestion(){
+    $("#question-field").empty();
     //Set question
     gameQuestion = questionBank.pop();
 
@@ -160,19 +160,15 @@ function setQuestion(){
     displayQuestion(gameQuestion);
 }
 
-// function gameStart(){
-//     setQuestion();
-// }
-
+//Once user has clicked game field to start game
 $("#question-field").on("click", function(){
     if(gameState == 1){
         $("#question-field").empty();
         setQuestion();
-        // gameState = 2;
-        // $("#game-state").text("Game state: " + gameState);
     }
 });
 
+//Resets game after Game Over
 $("#start-button").on("click", function(){
     if(gameState == 99){
         resetGame();
@@ -181,65 +177,90 @@ $("#start-button").on("click", function(){
 
 
 function endGame(){
+    $("#question-field").empty();
+
     //Add final game values to Game Over screen
     $("#question-field").append(addToField("h2", "", "FINAL SCORE"));
     $("#question-field").append(addToField("h2", "", "Correct: " + rightCount));
     $("#question-field").append(addToField("h2", "", "Wrong: " + wrongCount));
     $("#question-field").append(addToField("h2", "", "Click the Restart button to try again!"));
 
-    $("#game-result").text("GAME OVER");
+    $("#question-field").append(addToField("h2", "", "GAME OVER"));
+    // $("#game-result").text("GAME OVER");
     gameState = 99;
     $("#game-state").text("Game state: " + gameState);
 }
 
-function showAnswer(){
-    $("#question-field").empty();
-    if(gameState == 4){
+
+function resolveAnswer(){
+    //Show answer state
+    if(gameState == 3){
+        $("#question-field").empty();
+
+        //Stops answer timer
+        clearTimeout(questionTimer);
+        
+        if(timeUp){
+            $("#question-field").append(addToField("h2", "", "Time's up!"));
+            // $("#game-result").text("Time's up...");
+        } else{
+            if(playerCorrect){
+                $("#question-field").append(addToField("h2", "", "You got it!"));
+                // $("#game-result").text("You got it!");
+            } else {
+                $("#question-field").append(addToField("h2", "", "Wrong answer..."));
+                // $("#game-result").text("Wrong answer...");
+            }
+        }
+
+        //Unused state / answer has been displayed
+        gameState = 4;
+        $("#game-state").text("Game state: " + gameState);
+
+        
+        //Show player correct answer and accompanying picture
+        $("#question-field").append(addToField("h2", "", "Correct answer: " + gameQuestion.correctAnswer()));
+
+        var answerPicture = $("<img>");
+        answerPicture.attr("src", gameQuestion.picture);
+        answerPicture.attr("alt", "Correct Answer");
+        answerPicture.addClass("answer-picture")
+
+        $("#question-field").append(answerPicture);
+
+        //If there are questions left, set next question - otherwise end game
         if(questionBank.length > 0){
             answerTimer = setTimeout(setQuestion, 1000 * 5);
         } else {
             answerTimer = setTimeout(endGame, 1000 * 5);
         }
-    }
-}
 
-function resolveAnswer(){
-    if(gameState == 3){
-        clearTimeout(questionTimer);
-        if(timeUp){
-            $("#game-result").text("Time's up...");
-        } else{
-            if(playerGuess){
-                $("#game-result").text("You got it!");
-            } else {
-                $("#game-result").text("Wrong answer...");
-            }
-        }
-        gameState = 4;
-        $("#game-state").text("Game state: " + gameState);
-        showAnswer();
     }
 }
 
 function gameAnswer(){
+    //When an answer is clicked
     if(gameState == 2){
         timeUp = false;
         var result;
+
+        //Store the clicked answer in 'guess'
         var guess = $(this).text();
         //console.log(guess);
-        //go through answers
+
         if(gameQuestion.correctAnswer() == guess){
             console.log("Correct!");
             rightCount++;
-            playerGuess = true;
+            playerCorrect = true;
         } else {
             console.log("You lose!");
             wrongCount++;
-            playerGuess = false;
+            playerCorrect = false;
         }
+
+        //Show answer state
         gameState = 3;
         $("#game-state").text("Game state: " + gameState);
-        //go to show-result state to select new question
         resolveAnswer();
     }
 }
